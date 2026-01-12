a4056c9ca68965394d25c4fb9135ae229366f8a4
San123san <147592258+San123san@users.noreply.github.com>
2026-01-12 09:52:11 +0530

    Medium Heap

---
+++ Kth Smallest.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int kthSmallest(vector<int> &arr, int k) {
+        // code here
+        priority_queue<int> pq;
+        
+        for(int num : arr){
+            pq.push(num);
+            
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+        
+        return pq.top();
+    }
+};
